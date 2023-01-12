aec15dbd41b541bb7107b1923f648ca6d6e7b05a
Siddhu517 <siddhusadadekar517@gmail.com>
2023-01-12 22:34:43 +0530

    first commit 2

--- client/src/component/Chat.js
+++ client/src/component/Chat.js
@@ -25,7 +25,7 @@ const Chat = () => {
     ]);
 
     //fetch data from server
-    const response = await fetch("http://localhost:5000", {
+    const response = await fetch(process.env.REACT_APP_PUBLIC_API, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
